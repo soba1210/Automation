7fd418b783b3af40b553a705c803887fe1b52109
siinc <soba1210@gmail.com>
2019-07-17 18:56:44 +0300

    lesson 0 test 3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,8 @@
-public class MainClass
-{
-    private int class_number = 20;
-    public int getClassNumber(){
-        return class_number;
+public class MainClass {
+    private String class_string = "Hello, world";
+
+    public String getClassString() {
+        return class_string;
     }
 
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,9 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MainClassTest extends MainClass{
+public class MainClassTest extends MainClass {
     @Test
-    public void testGetClassNumber(){
-        Assert.assertTrue("getClassNumber lower or equal 45", getClassNumber()>45);
+    public void testGetClassString() {
+        Assert.assertTrue("string doesn't contain Hello or hello", getClassString().contains("hello") || getClassString().contains("Hello"));
     }
 }
